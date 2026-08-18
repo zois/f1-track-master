690250ca32cd10248a8c7daefab81e3daeb1e12e
zois <zois.avgerinos@protonmail.com>
2026-08-19 00:30:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "f1-track-master-v1";
+const CACHE = "f1-track-master-v2";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
