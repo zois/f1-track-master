e1a12ff081c7133e98a904832729c16a5dd2c4f0
zois avgerinos <zois_avger@live.com>
2026-08-19 15:09:12 +0300

    update tracks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "f1-track-master-v4";
+const CACHE = "f1-track-master-v5";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
